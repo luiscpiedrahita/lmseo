4821a6cca9290a0efb68415442a6b19157d3eabe
luiscpiedrahita <luis1420@gmail.com>
2025-12-14 15:48:52 -0500

    Remove bedrock's block to Search engine indexing in produccion.

--- config/environments/production.php
+++ config/environments/production.php
@@ -13,4 +13,4 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
-Config::define('DISALLOW_INDEXING', true);
+// Config::define('DISALLOW_INDEXING', true);
